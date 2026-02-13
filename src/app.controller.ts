a48dfd85d3e7c74cee25efeb970e4a50cad5f24d
zhaoxinkun <zhaoxinkun1124@gmail.com>
2026-02-13 11:26:48 +0800

    Revert "@nestjs/config"
    This reverts commit b90aafade413bcd06477c3735db08589f36993b8.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly configService: ConfigService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    const db = this.configService.get('DB');
-    console.log('🚀 ~ getHello ~ db: ', db);
     return this.appService.getHello();
   }
 }
